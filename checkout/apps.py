f78ef8be6417cb2f249b6214e7e8f3dd9d0697c3
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-12-20 19:23:35 +0000

    Create storage and calculation functions

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CheckeoutConfig(AppConfig):
+class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkeout'
+    name = 'checkout'
