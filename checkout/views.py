870695c1e8e70eea7cd46fe3a781b705ff079e53
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-12-29 22:35:41 +0100

    Setting up stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -15,5 +15,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SgsoY4K4yibvJ9oSngQExpPa7NoQuKNcdm6SWivaPe5ZIf8Zx1EL4kkGG23YHdyCVpeoEnZRNYyEI7UQYCDy60X0066nJGQ1Z',
+        'client_secret': 'test client secret'
     }
     return render(request, template, context)
