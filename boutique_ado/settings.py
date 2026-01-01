3155e31f573a70a21aaf1e1cf276cee5c9da1b7a
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2026-01-01 20:43:24 +0100

    Added checkout form data caching in payment intent

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = 'django-insecure-j(r2g*ii7#mm^hsp2!@4v!t94nua=lw0o$1^6@!0+8loms_lw8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -182,6 +185,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -8,12 +9,26 @@ class StripeWH_Handler:
 
     def handle_event(self, event):
         """
-        Handle a generic/unknown/unexpected webhook event.
-
-        :param event: The event data from Stripe
-        :return: HttpResponse indicating receipt of the event
+        Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
-            status=200
-        )
+            status=200)
+    
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        print(intent)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+    
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
